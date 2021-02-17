7a7b580f246b954e15fb65bafe6e1a6963acf1ea
Rizky-del <rizkydwi603@gmail.com>
2021-02-17 12:36:00 +0700

    Menambahkan paragraph

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Hello world</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum dolore ab aliquam earum ad inventore impedit sequi ea, aspernatur qui eius porro, quaerat, soluta nobis eligendi saepe eaque officia beatae!</p>
 </body>
 </html>
