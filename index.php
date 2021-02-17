ff1a45253942d858c4215dc3e24a869292fbe56b
Rizky-del <rizkydwi603@gmail.com>
2021-02-17 12:42:39 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,10 @@
 <body>
     <h1>Hello world</h1>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum dolore ab aliquam earum ad inventore impedit sequi ea, aspernatur qui eius porro, quaerat, soluta nobis eligendi saepe eaque officia beatae!</p>
+
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
